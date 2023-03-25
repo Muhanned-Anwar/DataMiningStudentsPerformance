65017b64baaf19bc0c0e9f3772abb9093db9b490
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-03-25 04:38:07 +0300

    Visualization for data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
 
 # Load and prepare data
 df = pd.read_csv('StudentsPerformance.csv')
@@ -18,7 +19,8 @@ df = pd.concat([df, dum_df], axis=1)
 df['score'] = df[['math score', 'reading score', 'writing score']].mean(axis=1)
 
 # Drop unnecessary columns
-df.drop(['gender', 'lunch', 'test preparation course', 'math score', 'reading score', 'writing score', 'race/ethnicity', 'parental level of education'], axis=1, inplace=True)
+df.drop(['gender', 'lunch', 'test preparation course', 'math score', 'reading score', 'writing score', 'race/ethnicity',
+         'parental level of education'], axis=1, inplace=True)
 
 # Save preprocessed data to a new CSV file
 df.to_csv('new_data.csv', index=False)
@@ -26,3 +28,8 @@ df.to_csv('new_data.csv', index=False)
 # Split data into features and target
 X = df.drop('score', axis=1)
 y = df['score']
+
+# Visualization for data
+plt.scatter(df['enc_gender'], y, c='r')
+plt.xticks(range(0, 2))
+plt.show()
