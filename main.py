cde65c48f6b80eb5d0aa9c157b96f630336ad845
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-03-25 04:38:46 +0300

    Linear Regression Classification

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 # Load and prepare data
 df = pd.read_csv('StudentsPerformance.csv')
@@ -33,3 +37,12 @@ y = df['score']
 plt.scatter(df['enc_gender'], y, c='r')
 plt.xticks(range(0, 2))
 plt.show()
+
+# Linear Regression Classification
+model = LinearRegression()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+error = np.sqrt(mean_squared_error(y_pred=y_pred, y_true=y_test))
+print(f"RMSE: {error}")
+print(f"R-squared: {model.score(X_test, y_test)}")
